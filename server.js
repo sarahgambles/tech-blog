062e98f441fd4996caddd50f9e85cb1cf9c3f8b3
Sarah Gambles <sarahgambles@Sarahs-MacBook-Pro.local>
2021-01-03 19:12:38 -0700

    added handlebars, seeds and edited routes

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const path = require('path');
-const sequelize = require('./config/connection');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
-
-const session = require('express-session');
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
     secret: 'Super secret secret',
@@ -21,14 +21,20 @@ const sess = {
 
 app.use(session(sess));
 
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({});
+
 const routes = require('./controllers');
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./controllers/'));
 
 app.use(routes);
 
